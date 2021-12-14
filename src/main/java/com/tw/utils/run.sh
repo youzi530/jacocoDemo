870b9417d92ebd68a50d2f3607913e132e5bb89e
youzi530 <2676272310@qq.com>
2021-12-14 15:59:23 +0800

    add sh

--- src/main/java/com/tw/utils/run.sh
+++ src/main/java/com/tw/utils/run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk
+PATH=${JAVA_HOME}/bin:$PATH
+CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar
+export JAVA_HOME
+export PATH
+export CLASSPATH
+
+echo "CLASSPATH"
+
 ORDER="$1"
 if [ "$ORDER" = "1" ]; then
  java CSVReader
